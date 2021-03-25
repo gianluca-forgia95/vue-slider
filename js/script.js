5e52b8243bf55a2efd1165b38b790d1f0915b525
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-25 15:03:39 +0100

    Inserisco le immagini nel data e le metto in pagina

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 var root = new Vue({
 el: '#root',
 data: {
-
-
+    images: [
+    "img/verdone.jpg",
+    "img/proietti.jpg",
+    "img/sordi.jpg",
+    "img/desica.jpg",
+    ],
+    imgIndex: 0
     },
    methods: {
-
+    nexImg: function() {
+      this.imgIndex++;
+    }
    }
 
 });
